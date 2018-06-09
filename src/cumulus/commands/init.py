99a7db88e62d9bb821a27d1a2b8742a734beabc8
Gurjyot <gurjyot.g@gmail.com>
2018-06-09 12:49:18 -0700

    Keep all existing Docker-Compose configs. Not just services

--- src/cumulus/commands/init.py
+++ src/cumulus/commands/init.py
@@ -12,10 +12,12 @@ class Init(Base):
             'version': Base.DOCKER_COMPOSE_VERSION,
             'services': {}
         }
+        self.already_started = []
         new_services = self.options['<service>']
         clean = self.options['--clean']
         for service in new_services:
             if(os.path.exists('./'+service) and not clean):
+                self.already_started.append(service)
                 continue
             if service in Base.WEB_APP:
                 doc['services'][service] = {
@@ -32,16 +34,24 @@ class Init(Base):
         #Load yml if exists and add to doc
         if(os.path.exists('docker-compose.yml') and os.stat('docker-compose.yml').st_size != 0):
             compose_tree = yaml.load(open('docker-compose.yml', 'r'))
-            existing_services = compose_tree['services']
-            
-            for service in existing_services:
-                if service not in doc['services']:
-                    doc['services'][service] = existing_services[service]
-            
+
+            for config_attribute in compose_tree:
+                if config_attribute not in doc: 
+                    doc[config_attribute] = compose_tree[config_attribute]
+
+                else:
+                    if isinstance(compose_tree[config_attribute], dict): 
+                        for item in compose_tree[config_attribute]: 
+                            doc[config_attribute][item] = compose_tree[config_attribute][item]
+                    else:
+                        doc[config_attribute] = compose_tree[config_attribute]
+        
         with open('docker-compose.yml', 'w') as outfile:
                 yaml.dump(doc, outfile, default_flow_style=False)   
 
     def run(self):
         self.init_docker_compose()
-        # for service in self.options['<service>']:
-            # Base.start_container(self.__class__.__name__, service)
+
+        for service in self.options['<service>']:
+            if service not in self.already_started:
+                # Base.start_container(self.__class__.__name__, service)
