5863b9c136fba8d4e814ccdd71c20bb8abf537ae
fattouche <alex.fattouche@gmail.com>
2018-06-08 17:24:44 -0700

    up for start and run for init

--- src/service_images/django/docker_entrypoint.sh
+++ src/service_images/django/docker_entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-if [[ $1 == "Init" ]]
+if [ "$1" == "INIT" ]
 then
   # User's cumulus config and code is mounted into /cumulus
   cd /cumulus
   mkdir django
-  cd django 
+  cd django
 
   django-admin startproject cumulus_web_app
-elif [[ $1 == "Start" ]]
-then
+else
   cd /cumulus
   cd django/cumulus_web_app
 
