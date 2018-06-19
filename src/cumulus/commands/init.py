f0b9d7a8489dfdb835c1da18d74749e1232e7fd3
gurjy0t <gurjyot.g@gmail.com>
2018-06-18 18:48:36 -0700

    Compose file (#18)
    * Keep all existing Docker-Compose configs. Not just services

--- src/cumulus/commands/init.py
+++ src/cumulus/commands/init.py
@@ -13,11 +13,15 @@ class Init(Base):
             'version': DOCKER_COMPOSE_VERSION,
             'services': {}
         }
+
+        self.already_started = []
         new_services = self.options['<service>']
         clean = self.options['--clean']
         for service in new_services:
             if(os.path.exists('./'+service) and not clean):
+                self.already_started.append(service)
                 continue
+                
             if service in WEB_APP:
                 doc['services'][service] = {
                     'image': DOCKER_HUB + service,
@@ -29,19 +33,29 @@ class Init(Base):
                     'image': service
                 }
 
-        # Load yml if exists and add to doc
+
+  
+        #Load yml if exists and add to doc
         if(os.path.exists('docker-compose.yml') and os.stat('docker-compose.yml').st_size != 0):
             compose_tree = yaml.load(open('docker-compose.yml', 'r'))
-            existing_services = compose_tree['services']
 
-            for service in existing_services:
-                if service not in doc['services']:
-                    doc['services'][service] = existing_services[service]
+            for config_attribute in compose_tree:
+                if config_attribute not in doc: 
+                    doc[config_attribute] = compose_tree[config_attribute]
 
+                else:
+                    if isinstance(compose_tree[config_attribute], dict): 
+                        for item in compose_tree[config_attribute]: 
+                            doc[config_attribute][item] = compose_tree[config_attribute][item]
+                    else:
+                        doc[config_attribute] = compose_tree[config_attribute]
+        
         with open('docker-compose.yml', 'w') as outfile:
-            yaml.dump(doc, outfile, default_flow_style=False)
+                yaml.dump(doc, outfile, default_flow_style=False) 
 
     def run(self):
         self.init_docker_compose()
         for service in self.options['<service>']:
-            init_container(service)
+            if service not in self.already_started:
+                init_container(service)
+
