b3577fd26d89ecd6286826e019ae8f8c0c249a28
LeeZeitz <andrew.l.zeitz@gmail.com>
2018-07-17 16:43:04 -0700

    bare bones ruby/rails image

---
+++ src/service_images/rails/cumulus-rails-docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$1" == INIT ]
+then
+    cd /cumulus
+
+    if [ ! -d "rails" ]
+    then
+        mkdir rails
+    fi
+
+    if [ ! -d "rails/${CUMULUS_PROJECT_NAME}" ]
+    then
+        cd rails
+        rails new ${CUMULUS_PROJECT_NAME}# -d mysql
+    fi
+fi
+
+mysql-server mysql-client
