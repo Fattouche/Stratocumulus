4515ee27a78192c282af816aad4eb8571bdf03ad
LeeZeitz <andrew.l.zeitz@gmail.com>
2018-07-19 18:31:07 -0700

    add bundle installs to entrypoint

--- src/service_images/rails/cumulus-rails-docker-entrypoint.sh
+++ src/service_images/rails/cumulus-rails-docker-entrypoint.sh
@@ -13,9 +13,11 @@ then
     then
         cd rails
         rails new ${CUMULUS_PROJECT_NAME}
+        bundle install
     fi
 
 else
     cd "/cumulus/rails/${CUMULUS_PROJECT_NAME}"
+    bundle install
     exec "$@"
 fi
